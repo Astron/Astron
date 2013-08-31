443ffc3b85f58f3e5b07cd628641fac95fb1dda1
Sam Edwards <CFSworks@gmail.com>
2013-08-31 12:22:27 -0600

    Begin work on MD unit tests.

---
+++ test/common.py
@@ -0,0 +1,95 @@
+import subprocess
+import tempfile
+import struct
+import socket
+import os
+
+__all__ = ['Daemon', 'Datagram', 'MDConnection']
+
+class Daemon(object):
+    DAEMON_PATH = './openotpd'
+
+    def __init__(self, config):
+        self.config = config
+
+        self.daemon = None
+        self.config_file = None
+
+    def start(self):
+        cfg, self.config_file = tempfile.mkstemp()
+        os.write(cfg, self.config)
+        os.close(cfg)
+
+        self.daemon = subprocess.Popen([self.DAEMON_PATH, self.config_file])
+
+    def stop(self):
+        self.daemon.kill()
+        os.remove(self.config_file)
+
+DATATYPES = {
+    'int8': '<b',
+    'uint8': '<B',
+    'int16': '<h',
+    'uint16': '<H',
+    'int32': '<i',
+    'uint32': '<I',
+    'int64': '<q',
+    'uint64': '<Q',
+}
+
+class Datagram(object):
+    def __init__(self, data=b''):
+        self._data = data
+
+        def make_adder(v):
+            def adder(data):
+                self.add_raw(struct.pack(v, data))
+            return adder
+
+        for k,v in DATATYPES.items():
+            adder = make_adder(v)
+            setattr(self, 'add_' + k, adder)
+
+    def add_raw(self, data):
+        self._data += data
+
+    def add_string(self, string):
+        self.add_uint16(len(string))
+        self.add_raw(string)
+
+    def get_data(self):
+        return self._data
+
+class MDConnection(object):
+    def __init__(self, sock):
+        self.s = sock
+        self.s.settimeout(0.1)
+
+    def send(self, datagram):
+        data = datagram.get_data()
+        msg = struct.pack('<H', len(data)) + data
+        self.s.send(msg)
+
+    def _read(self):
+        try:
+            length = struct.unpack('<H', self.s.recv(2))[0]
+        except socket.error:
+            return None
+
+        result = ''
+        while len(result) < length:
+            result += self.s.recv(length - len(result))
+
+        return result
+
+    def close(self):
+        self.s.close()
+
+    def flush(self):
+        while self._read(): pass
+
+    def expect(self, datagram):
+        return self._read() == datagram.get_data()
+
+    def expect_none(self):
+        return self._read() == None
---
+++ test/test_messagedirector.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python2
+import unittest
+from socket import *
+
+from common import *
+
+CONFIG = """\
+messagedirector:
+    bind: 127.0.0.1:57123
+    connect: 127.0.0.1:57124
+"""
+
+class TestMessageDirector(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        listener = socket(AF_INET, SOCK_STREAM)
+        listener.bind(('127.0.0.1', 57124))
+        listener.listen(1)
+        listener.settimeout(0.3)
+
+        cls.daemon = Daemon(CONFIG)
+        cls.daemon.start()
+
+        l, _ = listener.accept()
+        listener.close()
+        cls.l1 = MDConnection(l)
+
+        c = socket(AF_INET, SOCK_STREAM)
+        c.connect(('127.0.0.1', 57123))
+        cls.c1 = MDConnection(c)
+        c = socket(AF_INET, SOCK_STREAM)
+        c.connect(('127.0.0.1', 57123))
+        cls.c2 = MDConnection(c)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.l1.close()
+        cls.c1.close()
+        cls.c2.close()
+        cls.daemon.stop()
+
+    def test_single(self):
+        self.l1.flush()
+
+        # Send a datagram...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(1234)
+        dg.add_uint64(4321)
+        dg.add_uint16(1337)
+        dg.add_string('HELLO')
+        self.c1.send(dg)
+
+        # Make sure the MD passes it upward.
+        self.assertTrue(self.l1.expect(dg))
+
+    def test_subscribe(self):
+        self.l1.flush()
+        self.c1.flush()
+        self.c2.flush()
+
+        # Subscribe to a channel...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(4001)
+        dg.add_uint16(2001)
+        dg.add_uint64(12345654321)
+        self.c1.send(dg)
+        self.assertTrue(self.c1.expect_none())
+        # Make sure the MD subscribes to its parent.
+        self.assertTrue(self.l1.expect(dg))
+
+        # Send a test datagram on second connection...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(12345654321)
+        dg.add_uint64(0)
+        dg.add_uint16(1234)
+        dg.add_uint32(0xDEADBEEF)
+        self.c2.send(dg)
+        self.assertTrue(self.c1.expect(dg))
+        # MD will, of course, relay this datagram upward.
+        self.assertTrue(self.l1.expect(dg))
+
+        # Subscribe on the second connection...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(4001)
+        dg.add_uint16(2001)
+        dg.add_uint64(12345654321)
+        self.c2.send(dg)
+        self.assertTrue(self.c2.expect_none())
+        # MD should not ask for the channel a second time.
+        self.assertTrue(self.l1.expect_none())
+
+        # Send a test datagram on first connection...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(12345654321)
+        dg.add_uint64(0)
+        dg.add_uint16(1234)
+        dg.add_uint32(0xDEADBEEF)
+        self.c1.send(dg)
+        self.assertTrue(self.c2.expect(dg)) # Should be relayed to second.
+        self.assertTrue(self.l1.expect(dg)) # Should be sent upward.
+        self.assertTrue(self.c1.expect_none()) # Should NOT be echoed back.
+
+        # Unsubscribe on the first connection...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(4001)
+        dg.add_uint16(2002)
+        dg.add_uint64(12345654321)
+        self.c1.send(dg)
+        self.assertTrue(self.c1.expect_none())
+        self.assertTrue(self.c2.expect_none())
+        # MD should NOT unsubscribe from parent!
+        self.assertTrue(self.l1.expect_none())
+
+        # Send another test datagram on second connection...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(12345654321)
+        dg.add_uint64(0)
+        dg.add_uint16(1234)
+        dg.add_uint32(0xDEADBEEF)
+        self.c2.send(dg)
+        self.assertTrue(self.l1.expect(dg)) # Should be sent upward.
+        self.assertTrue(self.c2.expect_none()) # Should NOT be relayed.
+        self.assertTrue(self.c1.expect_none()) # Should NOT be echoed back.
+
+        # Unsubscribe on the second connection...
+        dg = Datagram()
+        dg.add_uint8(1)
+        dg.add_uint64(4001)
+        dg.add_uint16(2002)
+        dg.add_uint64(12345654321)
+        self.c2.send(dg)
+        self.assertTrue(self.c1.expect_none())
+        self.assertTrue(self.c2.expect_none())
+        # MD should unsubscribe from parent.
+        self.assertTrue(self.l1.expect(dg))
+
+if __name__ == '__main__':
+    unittest.main()
