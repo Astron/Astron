d98ea2f891940a0251a470a8373dfc1b7df3b036
Sam Edwards <CFSworks@gmail.com>
2013-08-31 11:48:42 -0600

    Add an extremely simple "sanity test" to the test suite.

---
+++ test/test_sanity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+import unittest
+
+class TestSanity(unittest.TestCase):
+    def setUp(self):
+        self.right_answer = 4
+
+    def test_sanity(self):
+        self.assertEqual(2 + 2, self.right_answer)
+
+if __name__ == '__main__':
+    unittest.main()
