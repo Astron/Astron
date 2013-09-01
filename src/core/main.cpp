61aca768f4b2feace9aaba28727e55ea19f4f04a
Maverick Mosher <MMavipc@hostedyou.com>
2013-09-01 14:06:54 -0700

    Fixed small memory leaks

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -8,10 +8,6 @@ int main(int argc, char *argv[])
 {
 	std::string cfg_file;
 
-	gConfig = new ConfigFile;
-	gDCF = new DCFile;
-
-
 	//TODO: Perhaps verbosity should be specified via command-line switch?
 	if (argc < 2)
 	{
@@ -28,13 +24,11 @@ int main(int argc, char *argv[])
 			}
 			else if (strcmp(argv[i], "-log") == 0 && i + 1 < argc)
 			{
+				delete gLogger;
 				gLogger = new Logger(argv[++i]);
 			}
 		}
 	}
-
-	if(!gLogger)
-		gLogger = new Logger;
 
 	gLogger->info() << "Loading configuration file..." << std::endl;
 
