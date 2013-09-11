5adc005b9bd2a11f4491a6e3cc959e642343abc4
Sam Edwards <CFSworks@gmail.com>
2013-09-11 17:12:16 -0600

    Datagram: Add constructor to make a Datagram from a buffer+length.

--- src/util/Datagram.h
+++ src/util/Datagram.h
@@ -31,6 +31,11 @@ public:
 		memcpy(buf, data.c_str(), data.length());
 	}
 
+	Datagram(const char *data, size_t length) : buf(new char[length]), buf_size(length), buf_end(length)
+	{
+		memcpy(buf, data, length);
+	}
+
 	Datagram(unsigned long long to_channel, unsigned long long from_channel, unsigned short message_type) : buf(new char[64]), buf_size(64), buf_end(0)
 	{
 		add_server_header(to_channel, from_channel, message_type);
