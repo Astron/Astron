db4c9383112ec39996034df2954581d409c5bd78
Sam Edwards <CFSworks@gmail.com>
2013-09-11 13:34:45 -0600

    EventLogger: Add role factory and log output support.

--- src/eventlogger/EventLogger.cpp
+++ src/eventlogger/EventLogger.cpp
@@ -1,6 +1,9 @@
+#include "core/RoleFactory.h"
 #include "EventLogger.h"
 
-EventLogger::EventLogger(RoleConfig roleconfig) : Role(roleconfig)
+EventLogger::EventLogger(RoleConfig roleconfig) : Role(roleconfig), m_log("eventlogger", "Event Logger")
 {
 
 }
+
+RoleFactoryItem<EventLogger> el_fact("eventlogger");
--- src/eventlogger/EventLogger.h
+++ src/eventlogger/EventLogger.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include "core/global.h"
 #include "util/Role.h"
 
 class EventLogger : public Role
 {
 public:
 	EventLogger(RoleConfig roleconfig);
+
+	void handle_datagram(Datagram &in_dg, DatagramIterator &dgi) { } // Doesn't take DGs.
+
+private:
+	LogCategory m_log;
 };
