1f0588e91ce32a38677809588dd837a2c1a2ef67
Maverick Mosher <MMavipc@hostedyou.com>
2013-09-19 19:19:11 -0700

    ClientAgent: Style conformance

--- src/clientagent/ClientAgent.cpp
+++ src/clientagent/ClientAgent.cpp
@@ -10,26 +10,26 @@ static ConfigVariable<std::string> bind_addr("bind", "0.0.0.0:7198");
 
 class ClientAgent : public Role
 {
-public:
-	ClientAgent(RoleConfig roleconfig) : Role(roleconfig)
-	{
-		std::stringstream ss;
-		ss << "Client Agent (" << bind_addr.get_rval(roleconfig) << ")";
-		m_log = new LogCategory("clientagent", ss.str());
-	}
+	public:
+		ClientAgent(RoleConfig roleconfig) : Role(roleconfig)
+		{
+			std::stringstream ss;
+			ss << "Client Agent (" << bind_addr.get_rval(roleconfig) << ")";
+			m_log = new LogCategory("clientagent", ss.str());
+		}
 
-	~ClientAgent()
-	{
-		delete m_log;
-	}
+		~ClientAgent()
+		{
+			delete m_log;
+		}
 
-	void handle_datagram(Datagram &in_dg, DatagramIterator &dgi)
-	{
+		void handle_datagram(Datagram &in_dg, DatagramIterator &dgi)
+		{
 
-	}
+		}
 
-private:
-	LogCategory *m_log;
+	private:
+		LogCategory *m_log;
 };
 
 static RoleFactoryItem<ClientAgent> ca_fact("clientagent");
