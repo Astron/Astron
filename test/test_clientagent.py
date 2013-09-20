76ea148ba73936aa56712fae8fdc6d75a11e8e28
Sam Edwards <CFSworks@gmail.com>
2013-09-19 19:06:02 -0600

    tests: Add Client Agent test for anonymous UberDOGs.

--- test/test_clientagent.py
+++ test/test_clientagent.py
@@ -38,6 +38,9 @@ class TestClientAgent(unittest.TestCase):
         s.connect(('127.0.0.1', 57123))
         cls.server = MDConnection(s)
 
+        cls.server.send(Datagram.create_add_channel(1234))
+        cls.server.send(Datagram.create_add_channel(1235))
+
     @classmethod
     def tearDownClass(cls):
         cls.server.close()
@@ -97,6 +100,55 @@ class TestClientAgent(unittest.TestCase):
         self.assertTrue(client.expect(dg))
 
         client.close()
+
+    def test_anonymous(self):
+        # Connect and hello:
+        client = self.connect()
+        dg = Datagram()
+        dg.add_uint16(CLIENT_HELLO)
+        dg.add_uint32(DC_HASH)
+        dg.add_string(VERSION)
+        client.send(dg)
+        dg = Datagram()
+        dg.add_uint16(CLIENT_HELLO_RESP)
+        self.assertTrue(client.expect(dg))
+
+        # Now, we should be able to send an update to the anonymous UD...
+        dg = Datagram()
+        dg.add_uint16(CLIENT_OBJECT_UPDATE_FIELD)
+        dg.add_uint32(1234)
+        dg.add_uint16(request)
+        dg.add_string('[month of winter coolness]*3')
+        client.send(dg)
+
+        # Nothing should happen on the client...
+        self.assertTrue(client.expect_none())
+
+        # And the server should see the update...
+        dgi = DatagramIterator(self.server.recv())
+        self.assertEqual(dgi.read_uint8(), 1)
+        self.assertEqual(dgi.read_uint64(), 1234)
+        dgi.read_uint64() # Sender ID; we don't know this.
+        self.assertEqual(dgi.read_uint16(), STATESERVER_OBJECT_UPDATE_FIELD)
+        self.assertEqual(dgi.read_uint32(), 1234)
+        self.assertEqual(dgi.read_uint16(), request)
+        self.assertEqual(dgi.read_string(), '[month of winter coolness]*3')
+
+        # However, if we send an update to the non-anonymous UD...
+        dg = Datagram()
+        dg.add_uint16(CLIENT_OBJECT_UPDATE_FIELD)
+        dg.add_uint32(1235)
+        dg.add_uint16(foo)
+        dg.add_uint8(1)
+        dg.add_uint8(5)
+        dg.add_uint8(7)
+        client.send(dg)
+
+        # Nothing should happen on the server...
+        self.assertTrue(self.server.expect_none())
+
+        # And the client should get booted.
+        self.assertDisconnect(client, CLIENT_DISCONNECT_ANONYMOUS_VIOLATION)
 
 if __name__ == '__main__':
     unittest.main()
