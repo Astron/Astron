831ae4805762449197a3c00e0642d55907305fac
kestred <kestred@riotcave.com>
2013-09-30 12:31:55 -0600

    Tests: Add DBSS unittest for set_zone.

--- test/test_dbss.py
+++ test/test_dbss.py
@@ -127,6 +127,103 @@ class TestStateServer(unittest.TestCase):
         # Should recieve no stateserver object response
         self.shard.expect_none()
 
+    def test_set_zone(self):
+        self.database.flush()
+        self.shard.flush()
+        self.shard.send(Datagram.create_add_channel(80000<<32|100))
+        self.shard.send(Datagram.create_add_channel(80000<<32|101))
+
+        # Enter an object into ram from the disk by setting its zone
+        dg = Datagram.create([9001], 5, STATESERVER_OBJECT_SET_ZONE)
+        dg.add_uint32(80000) # Parent
+        dg.add_uint32(100) # Zone
+        self.shard.send(dg)
+
+        # Expect values to be retrieved from database
+        dg = self.database.recv()
+        dgi = DatagramIterator(dg)
+        self.assertTrue(dgi.matches_header([200], 9001, DATABASE_OBJECT_GET_ALL, 4+4))
+        context = dgi.read_uint32() # Get context
+        self.assertTrue(dgi.read_uint32() == 9001) # object Id
+
+        # Send back to the DBSS with some required values
+        dg = Datagram.create([9001], 200, DATABASE_OBJECT_GET_ALL_RESP)
+        dg.add_uint32(context)
+        dg.add_uint8(SUCCESS)
+        dg.add_uint16(DistributedTestObject5)
+        dg.add_uint16(2)
+        dg.add_uint16(setRDB3)
+        dg.add_uint32(3117)
+        dg.add_uint16(setRDbD5)
+        dg.add_uint8(97)
+        self.database.send(dg)
+
+        # See if it announces its entry into 100.
+        dg = Datagram.create([80000<<32|100], 9001, STATESERVER_OBJECT_ENTERZONE_WITH_REQUIRED)
+        dg.add_uint32(80000) # Parent
+        dg.add_uint32(100) # Zone
+        dg.add_uint16(DistributedTestObject5)
+        dg.add_uint32(9000) # ID
+        dg.add_uint32(3117) # setRequired1
+        dg.add_uint32(32144123) # setRDB3
+        dg.add_uint8(97) # setRDbD5
+        self.shard.expect(dg)
+
+        # Move an object in ram to a different zone
+        dg = Datagram.create([9001], 5, STATESERVER_OBJECT_SET_ZONE)
+        dg.add_uint32(80000) # Parent
+        dg.add_uint32(101) # Zone
+        self.shard.send(dg)
+
+        # See if it announces its departure from 100...
+        expected = []
+        dg = Datagram.create([80000<<32|100], 5, STATESERVER_OBJECT_CHANGE_ZONE)
+        dg.add_uint32(9001)
+        dg.add_uint32(80000)
+        dg.add_uint32(101)
+        dg.add_uint32(80000)
+        dg.add_uint32(100)
+        expected.append(dg)
+        # ...and its entry into 101.
+        dg = Datagram.create([80000<<32|101], 9001, STATESERVER_OBJECT_ENTERZONE_WITH_REQUIRED)
+        dg.add_uint32(80000) # Parent
+        dg.add_uint32(101) # Zone
+        dg.add_uint16(DistributedTestObject5)
+        dg.add_uint32(9001) # ID
+        dg.add_uint32(3117) # setRequired1
+        dg.add_uint32(32144123) # setRDB3
+        dg.add_uint8(97) # setRDbD5
+        expected.append(dg)
+
+        self.assertTrue(self.c.expect_multi(expected, only=True))
+
+        # Destroy our object in ram...
+        dg = Datagram.create([9001], 5, STATESERVER_OBJECT_DELETE_RAM)
+        dg.add_uint32(9001)
+        self.shard.send(dg)
+
+        # Object should announce its disappearance...
+        dg = Datagram.create([80000<<32|101], 9001, STATESERVER_OBJECT_DELETE_RAM)
+        dg.add_uint32(9001)
+        self.assertTrue(self.shard.expect(dg))
+
+        # Destroy our object on disk...
+        dg = Datagram.create([9001], 5, STATESERVER_OBJECT_DELETE_DISK)
+        dg.add_uint32(9001)
+        self.shard.send(dg)
+
+        # Object should announce its disappearance...
+        dg = Datagram.create([80000<<32|101], 9001, STATESERVER_OBJECT_DELETE_DISK)
+        dg.add_uint32(9001)
+        self.assertTrue(self.shard.expect(dg))
+
+        # Ignore database call, not testing that behavior here
+        self.database.flush()
+
+        # Clean up
+        self.shard.send(Datagram.create_remove_channel(80000<<32|100))
+        self.shard.send(Datagram.create_remove_channel(80000<<32|101))
+
     # Tests that the DBSS is listening to the entire range it was configured with
     #def test_subscribe(self):
     #    self.fail("Test not implemented.")
