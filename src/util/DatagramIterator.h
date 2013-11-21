25a6e181ff8f709475eca6450d439f75460e9786
kestred <kestred@riotcave.com>
2013-11-21 03:54:02 -0700

    Merge DatagramIterator changes from downstream libastron-cxx.

--- src/util/DatagramIterator.h
+++ src/util/DatagramIterator.h
@@ -6,12 +6,15 @@
 #include <fstream>
 #endif
 
+// A DatagramIteratorEOF is an exception that is thrown when attempting to read
+// past the end of a datagram.
 class DatagramIteratorEOF : public std::runtime_error
 {
 	public:
 		DatagramIteratorEOF(const string &what) : std::runtime_error(what) { }
 };
 
+// A DatagramIterator lets you step trough a datagram by reading a single value at a time.
 class DatagramIterator
 {
 	protected:
@@ -29,17 +32,21 @@ class DatagramIterator
 			};
 		}
 	public:
+		// constructor
 		DatagramIterator(const Datagram &dg, dgsize_t offset = 0) : m_dg(dg), m_offset(offset)
 		{
 			check_read_length(0); //shortcuts, yay
 		}
 
+		// read_bool reads the next byte from the datagram and returns either false or true.
 		bool read_bool()
 		{
 			uint8_t val = read_uint8();
 			return val != false; // returns either 1 or 0
 		}
 
+		// read_uint8 reads a byte from the datagram,
+		// returning an unsigned 8-bit integer in native endianness.
 		uint8_t read_uint8()
 		{
 			check_read_length(1);
@@ -48,6 +55,8 @@ class DatagramIterator
 			return r;
 		}
 
+		// read_uint16 reads 2 bytes from the datagram,
+		// returning an unsigned 16-bit integer in native endianness.
 		uint16_t read_uint16()
 		{
 			check_read_length(2);
@@ -56,6 +65,8 @@ class DatagramIterator
 			return r;
 		}
 
+		// read_uint32 reads 4 bytes from the datagram,
+		// returning an unsigned 32-bit integer in native endianness.
 		uint32_t read_uint32()
 		{
 			check_read_length(4);
@@ -64,6 +75,8 @@ class DatagramIterator
 			return r;
 		}
 
+		// read_uint64 reads 8 bytes from the datagram,
+		// returning an unsigned 64-bit integer in native endianness.
 		uint64_t read_uint64()
 		{
 			check_read_length(8);
@@ -72,6 +85,16 @@ class DatagramIterator
 			return r;
 		}
 
+		// read_size reads a dgsize_t from the datagram.
+		dgsize_t read_size()
+		{
+			check_read_length(sizeof(dgsize_t));
+			dgsize_t r = *(dgsize_t*)(m_dg.get_data() + m_offset);
+			m_offset += sizeof(dgsize_t);
+			return r;
+		}
+
+		// read_channel reads a channel_t from the datagram.
 		channel_t read_channel()
 		{
 			check_read_length(sizeof(channel_t));
@@ -80,6 +103,7 @@ class DatagramIterator
 			return r;
 		}
 
+		// read_doid reads a doid_t from the datagram.
 		doid_t read_doid()
 		{
 			check_read_length(sizeof(doid_t));
@@ -88,6 +112,7 @@ class DatagramIterator
 			return r;
 		}
 
+		// read_zone reads a zone_t from the datagram.
 		zone_t read_zone()
 		{
 			check_read_length(sizeof(zone_t));
@@ -96,14 +121,8 @@ class DatagramIterator
 			return r;
 		}
 
-		double read_float64()
-		{
-			check_read_length(8);
-			double r = *(double*)(m_dg.get_data() + m_offset);
-			m_offset += 8;
-			return r;
-		}
-
+		// read_float32 reads 4 bytes from the datagram,
+		// returning a 32-bit float in native endianness.
 		float read_float32()
 		{
 			check_read_length(4);
@@ -112,11 +131,21 @@ class DatagramIterator
 			return r;
 		}
 
+		// read_float64 reads 8 bytes from the datagram,
+		// returning a 64-bit float (double) in native endianness.
+		double read_float64()
+		{
+			check_read_length(8);
+			double r = *(double*)(m_dg.get_data() + m_offset);
+			m_offset += 8;
+			return r;
+		}
+
 		// read_string reads a string from the datagram in the format
-		//     {uint16 length; char[length] characters}.
+		//     {dgsize_t length; char[length] characters} and returns the character data.
 		std::string read_string()
 		{
-			uint16_t length = read_uint16();
+			dgsize_t length = read_size();
 			check_read_length(length);
 			std::string str((char*)(m_dg.get_data() + m_offset), length);
 			m_offset += length;
@@ -124,34 +153,21 @@ class DatagramIterator
 		}
 
 		// read_blob reads a blob from the datagram in the format
-		//     {uint16 length; uint8[length] binary}.
+		//     {dgsize_t length; uint8[length] binary} and returns the binary part.
 		std::vector<uint8_t> read_blob()
 		{
-			uint16_t length = read_uint16();
+			dgsize_t length = read_size();
 			return read_data(length);
 		}
 
-		// read_datagram reads a nested datagram from the datagram in the format
-		//     {dgsize_t length; uint8[length] binary}.
-		// This is also useful to read the whole datagram, because the beginning of the datagram
-		// looks exactly the same as a nested datagram.
-		std::vector<uint8_t> read_datagram()
+		// read_datagram reads a blob from the datagram and returns it as another datagram.
+		Datagram read_datagram()
 		{
-			// NOTE: datagrams are stored in other datagrams as blobs (which always have 16-bit size)
-			// therefore it is a temporary limitation of astron that the max post-remove size is 16-bit,
-            // even if you are using 32-bit datagram mode.
-			// TODO: Address this problem in either the protocol or  and document
-
-			/*
-			check_read_length(sizeof(dgsize_t));
-			dgsize_t length = *(dgsize_t*)(m_dg.get_data() + m_offset);
-			m_offset += sizeof(dgsize_t);
-			*/
-
-			uint16_t length = read_uint16();
-			return read_data(length);
+			dgsize_t length = read_size();
+			return Datagram(m_dg.get_data() + m_offset, length);
 		}
 
+		// read_data returns the next <length> bytes in the datagram.
 		std::vector<uint8_t> read_data(dgsize_t length)
 		{
 			check_read_length(length);
@@ -160,11 +176,13 @@ class DatagramIterator
 			return data;
 		}
 
+		// read_remainder returns a vector containing the rest of the bytes in the datagram.
 		std::vector<uint8_t> read_remainder()
 		{
 			return read_data(m_dg.size() - m_offset);
 		}
 
+		// unpack_field accepts a DCField type and reads the value of the field into a buffer.
 		void unpack_field(DCPackerInterface *field, std::vector<uint8_t> &buffer)
 		{
 			// If field is a fixed-sized type like uint, int, float, etc
@@ -212,6 +230,16 @@ class DatagramIterator
 			}
 		}
 
+		// unpack_field can also be called without a reference to unpack into a new buffer.
+		std::vector<uint8_t> unpack_field(DCPackerInterface *field)
+		{
+			std::vector<uint8_t> buffer;
+			unpack_field(field, buffer);
+			return buffer;
+		}
+
+		// skip_field can be used to seek past the packed field data for a DCField.
+		//     Throws DatagramIteratorEOF if it skips past the end of the datagram.
 		void skip_field(DCPackerInterface *field)
 		{
 			// Skip over fields with fixed byte size
@@ -254,9 +282,9 @@ class DatagramIterator
 			}
 		}
 
-		// get_msg_type returns the datagram's recipient count. Does not advance the offset.
+		// get_recipient_count returns the datagram's recipient count. Does not advance the offset.
 		// Should be used when the current offset needs to be saved and/or if the next field in the
-		//     datagram is not the recipient_count. If stepping through a fresh datagram, use read_uint8().
+		// datagram is not the recipient_count. If stepping through a fresh datagram, use read_uint8.
 		uint8_t get_recipient_count()
 		{
 			if(m_dg.size() > 0)
@@ -271,10 +299,10 @@ class DatagramIterator
 
 		// get_sender returns the datagram's sender. Does not advance the offset.
 		// Should be used when the current offset needs to be saved and/or if the next field in the
-		//     datagram is not the sender. If stepping through a fresh datagram, use read_uint64().
+		//     datagram is not the sender. If stepping through a fresh datagram, use read_channel.
 		channel_t get_sender()
 		{
-			uint16_t offset = m_offset; // save offset
+			dgsize_t offset = m_offset; // save offset
 
 			m_offset = 1 + get_recipient_count() * sizeof(channel_t); // seek sender
 			channel_t sender = read_channel(); // read sender
@@ -285,10 +313,10 @@ class DatagramIterator
 
 		// get_msg_type returns the datagram's message type. Does not advance the offset.
 		// Should be used when the current offset needs to be saved and/or if the next field in the
-		//     datagram is not the msg_type. If stepping through a fresh datagram, use read_uint16().
+		//     datagram is not the msg_type. If stepping through a fresh datagram, use read_uint16.
 		uint16_t get_msg_type()
 		{
-			uint16_t offset = m_offset; // save offset
+			dgsize_t offset = m_offset; // save offset
 
 			m_offset = 9 + get_recipient_count() * sizeof(channel_t); // seek message type
 			uint16_t msg_type = read_uint16(); // read message type
@@ -298,13 +326,13 @@ class DatagramIterator
 		}
 
 		// tell returns the current message offset in std::vector<uint8_t>
-		uint16_t tell()
+		dgsize_t tell()
 		{
 			return m_offset;
 		}
 
 		// get_remaining returns the number of unread bytes left
-		uint16_t get_remaining() const
+		dgsize_t get_remaining() const
 		{
 			return m_dg.size() - m_offset;
 		}
@@ -316,6 +344,7 @@ class DatagramIterator
 		}
 
 		// seek_payload seeks to immediately after the list of receivers
+		//     (typically, [channel_t sender, uint16_t msgtype, ...])
 		void seek_payload()
 		{
 			m_offset = 0; // Seek to start
