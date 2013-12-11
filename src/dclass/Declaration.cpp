4db6037707c9f02fe6f58f35f4c382b0bc9c15eb
kestred <kestred@riotcave.com>
2013-12-11 12:17:55 -0700

    dclass: First iteration revision of Declaration.

--- src/dclass/Declaration.cpp
+++ src/dclass/Declaration.cpp
@@ -1,94 +1,62 @@
-// Filename: dcDeclaration.cxx
-// Created by:  drose (18Jun04)
+// Filename: Declaration.cpp
+// Created by: drose (18 Jun, 2004)
 //
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
 // Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
 // All use of this software is subject to the terms of the revised BSD
 // license.  You should have received a copy of this license along
 // with this source code in a file named "LICENSE."
 //
-////////////////////////////////////////////////////////////////////
 
-#include "dcDeclaration.h"
+#include "Declaration.h"
+namespace dclass   // open namespace
+{
 
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCDeclaration::Destructor
-//       Access: Public, Virtual
-//  Description:
-////////////////////////////////////////////////////////////////////
-DCDeclaration::
-~DCDeclaration()
+// destructor
+Declaration::~Declaration()
 {
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCDeclaration::as_class
-//       Access: Published, Virtual
-//  Description:
-////////////////////////////////////////////////////////////////////
-DCClass *DCDeclaration::
-as_class()
+// as_class returns the same declaration pointer converted to a class
+//     pointer, if this is in fact a class; otherwise, returns NULL.
+Class* Declaration::as_class()
 {
-	return (DCClass *)NULL;
+	return (DCClass*)NULL;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCDeclaration::as_class
-//       Access: Published, Virtual
-//  Description:
-////////////////////////////////////////////////////////////////////
-const DCClass *DCDeclaration::
-as_class() const
+// as_class returns the same declaration pointer converted to a class
+//     pointer, if this is in fact a class; otherwise, returns NULL.
+const Class* Declaration::as_class() const
 {
-	return (DCClass *)NULL;
+	return (Class*)NULL;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCDeclaration::as_switch
-//       Access: Published, Virtual
-//  Description:
-////////////////////////////////////////////////////////////////////
-DCSwitch *DCDeclaration::
-as_switch()
+// as_switch returns the same declaration pointer converted to a switch
+//     pointer, if this is in fact a switch; otherwise, returns NULL.
+Switch* Declaration::as_switch()
 {
-	return (DCSwitch *)NULL;
+	return (Switch*)NULL;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCDeclaration::as_switch
-//       Access: Published, Virtual
-//  Description:
-////////////////////////////////////////////////////////////////////
-const DCSwitch *DCDeclaration::
-as_switch() const
+// as_switch returns the same declaration pointer converted to a switch
+//     pointer, if this is in fact a switch; otherwise, returns NULL.
+const Switch* Declaration::as_switch() const
 {
-	return (DCSwitch *)NULL;
+	return (Switch*)NULL;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function : DCDeclaration::output
-//       Access : Published, Virtual
-//  Description : Write a string representation of this instance to
-//                <out>.
-////////////////////////////////////////////////////////////////////
-void DCDeclaration::
-output(ostream &out) const
+// output writes a string representation of the declaration to an output stream
+void Declaration::output(ostream &out) const
 {
 	output(out, true);
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function : DCDeclaration::
-//       Access : Published
-//  Description : Write a string representation of this instance to
-//                <out>.
-////////////////////////////////////////////////////////////////////
-void DCDeclaration::
-write(ostream &out, int indent_level) const
+// write writes a string representation of the declaration to an output stream
+void Declaration::write(ostream &out, int indent_level) const
 {
 	write(out, false, indent_level);
 }
+
+
+} // close namespace dclass
--- src/dclass/Declaration.h
+++ src/dclass/Declaration.h
@@ -1,59 +1,59 @@
-// Filename: dcDeclaration.h
-// Created by:  drose (18Jun04)
+// Filename: Declaration.h
+// Created by: drose (18 Jun, 2004)
 //
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
 // Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
 // All use of this software is subject to the terms of the revised BSD
 // license.  You should have received a copy of this license along
 // with this source code in a file named "LICENSE."
 //
-////////////////////////////////////////////////////////////////////
 
-#ifndef DCDECLARATION_H
-#define DCDECLARATION_H
-
+#pragma once
 #include "dcbase.h"
+namespace dclass   // open namespace
+{
 
-class DCClass;
-class DCSwitch;
 
-////////////////////////////////////////////////////////////////////
-//       Class : DCDeclaration
-// Description : This is a common interface for a declaration in a DC
-//               file.  Currently, this is either a class or a typedef
-//               declaration (import declarations are still collected
-//               together at the top, and don't inherit from this
-//               object).  Its only purpose is so that classes and
-//               typedefs can be stored in one list together so they
-//               can be ordered correctly on output.
-////////////////////////////////////////////////////////////////////
-class EXPCL_DIRECT DCDeclaration
+// Forward declarations
+class Class;
+class Switch;
+
+// A Declaration is a common interface for a declaration in a DC file.
+//     Currently, this is either a class or a typedef declaration (import declarations are
+//     still collected together at the top, and don't inherit from this object).
+//     Its only purpose is so that classes and typedefs can be stored in one list
+//     together so they can be ordered correctly on output.
+class EXPCL_DIRECT Declaration
 {
 	public:
-		virtual ~DCDeclaration();
+		virtual ~Declaration();
 
 	PUBLISHED:
-		virtual DCClass *as_class();
-		virtual const DCClass *as_class() const;
-		virtual DCSwitch *as_switch();
-		virtual const DCSwitch *as_switch() const;
+		// as_class returns the same declaration pointer converted to a class
+		//     pointer, if this is in fact a class; otherwise, returns NULL.
+		virtual Class *as_class();
+		virtual const Class *as_class() const;
 
+		// as_switch returns the same declaration pointer converted to a switch
+		//     pointer, if this is in fact a switch; otherwise, returns NULL.
+		virtual Switch *as_switch();
+		virtual const Switch *as_switch() const;
+
+		// output and write output representations of the declaration to an output stream
 		virtual void output(ostream &out) const;
 		void write(ostream &out, int indent_level) const;
 
 	public:
+		// output and write output representations of the declaration to an output stream
 		virtual void output(ostream &out, bool brief) const = 0;
 		virtual void write(ostream &out, bool brief, int indent_level) const = 0;
 };
 
-INLINE ostream &operator << (ostream &out, const DCDeclaration &decl)
+inline ostream &operator << (ostream &out, const Declaration &decl)
 {
 	decl.output(out);
 	return out;
 }
 
-#endif
 
+} // close namespace dclass
