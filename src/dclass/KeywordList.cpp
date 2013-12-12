edc647ddd26b024f6fc5a889fe4894a6b28837a2
kestred <kestred@riotcave.com>
2013-12-12 07:09:14 -0700

    dclass: Initial revision of KeywordList.

--- src/dclass/KeywordList.cpp
+++ src/dclass/KeywordList.cpp
@@ -1,225 +1,138 @@
-// Filename: dcKeywordList.cxx
-// Created by:  drose (25Jul05)
+// Filename: KeywordList.cpp
+// Created by: drose (25 Jul, 2005)
 //
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
 // Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
 // All use of this software is subject to the terms of the revised BSD
 // license.  You should have received a copy of this license along
 // with this source code in a file named "LICENSE."
 //
-////////////////////////////////////////////////////////////////////
 
-#include "dcKeywordList.h"
-#include "dcKeyword.h"
-#include "hashGenerator.h"
+#include "KeywordList.h"
+#include "Keyword.h"
+#include "HashGenerator.h"
+namespace dclass   // open namespace dclass
+{
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::Constructor
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-DCKeywordList::
-DCKeywordList() :
-	_flags(0)
+
+// empty list constructor
+KeywordList::KeywordList() : m_flags(0)
 {
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::Copy Constructor
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-DCKeywordList::
-DCKeywordList(const DCKeywordList &copy) :
-	_keywords(copy._keywords),
-	_keywords_by_name(copy._keywords_by_name),
-	_flags(copy._flags)
+// copy constructor
+KeywordList::KeywordList(const KeywordList &copy) : m_keywords(copy.m_keywords),
+	m_keywords_by_name(copy.m_keywords_by_name), m_flags(copy.m_flags)
 {
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::Copy Assignment Operator
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-void DCKeywordList::
-operator = (const DCKeywordList &copy)
+// copy assignment operator
+void KeywordList::operator = (const KeywordList &copy)
 {
-	_keywords = copy._keywords;
-	_keywords_by_name = copy._keywords_by_name;
-	_flags = copy._flags;
+	m_keywords = copy.m_keywords;
+	m_keywords_by_name = copy.m_keywords_by_name;
+	m_flags = copy.m_flags;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::Destructor
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-DCKeywordList::
-~DCKeywordList()
+// destructor
+KeywordList::~KeywordList()
 {
-	nassertv(_keywords_by_name.size() == _keywords.size());
+	nassertv(m_keywords_by_name.size() == m_keywords.size());
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::has_keyword
-//       Access: Published
-//  Description: Returns true if this list includes the indicated
-//               keyword, false otherwise.
-////////////////////////////////////////////////////////////////////
-bool DCKeywordList::
-has_keyword(const string &name) const
+// has_keyword returns true if this list includes the indicated keyword, false otherwise.
+bool KeywordList::has_keyword(const string &name) const
 {
-	return (_keywords_by_name.find(name) != _keywords_by_name.end());
+	return (m_keywords_by_name.find(name) != m_keywords_by_name.end());
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::has_keyword
-//       Access: Published
-//  Description: Returns true if this list includes the indicated
-//               keyword, false otherwise.
-////////////////////////////////////////////////////////////////////
-bool DCKeywordList::
-has_keyword(const DCKeyword *keyword) const
+// has_keyword returns true if this list includes the indicated keyword, false otherwise.
+bool KeywordList::has_keyword(const Keyword *keyword) const
 {
 	return has_keyword(keyword->get_name());
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::get_num_keywords
-//       Access: Published
-//  Description: Returns the number of keywords in the list.
-////////////////////////////////////////////////////////////////////
-int DCKeywordList::
-get_num_keywords() const
+// get_num_keywords returns the number of keywords in the list.
+int KeywordList::get_num_keywords() const
 {
-	nassertr(_keywords_by_name.size() == _keywords.size(), 0);
-	return _keywords.size();
+	nassertr(m_keywords_by_name.size() == m_keywords.size(), 0);
+	return m_keywords.size();
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::get_keyword
-//       Access: Published
-//  Description: Returns the nth keyword in the list.
-////////////////////////////////////////////////////////////////////
-const DCKeyword *DCKeywordList::
-get_keyword(int n) const
+// get_keyword returns the nth keyword in the list.
+const Keyword *KeywordList::get_keyword(int n) const
 {
-	nassertr(n >= 0 && n < (int)_keywords.size(), NULL);
-	return _keywords[n];
+	nassertr(n >= 0 && n < (int)m_keywords.size(), NULL);
+	return m_keywords[n];
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::get_keyword_by_name
-//       Access: Published
-//  Description: Returns the keyword in the list with the indicated
-//               name, or NULL if there is no keyword in the list with
-//               that name.
-////////////////////////////////////////////////////////////////////
-const DCKeyword *DCKeywordList::
-get_keyword_by_name(const string &name) const
+// get_keyword_by_name returns the keyword in the list with the indicated name,
+//     or NULL if there is no keyword in the list with that name.
+const Keyword *KeywordList::get_keyword_by_name(const string &name) const
 {
-	KeywordsByName::const_iterator ni;
-	ni = _keywords_by_name.find(name);
-	if(ni != _keywords_by_name.end())
+	auto key_it = m_keywords_by_name.find(name);
+	if(key_it != m_keywords_by_name.end())
 	{
-		return (*ni).second;
+		return it->second;
 	}
 
 	return NULL;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::compare_keywords
-//       Access: Published
-//  Description: Returns true if this list has the same keywords
-//               as the other list, false if some keywords differ.
-//               Order is not considered important.
-////////////////////////////////////////////////////////////////////
-bool DCKeywordList::
-compare_keywords(const DCKeywordList &other) const
+// compare_keywords returns true if this list has the same keywords as the other list,
+//     false if some keywords differ. Order is not considered important.
+bool KeywordList::compare_keywords(const KeywordList &other) const
 {
-	return _keywords_by_name == other._keywords_by_name;
+	return m_keywords_by_name == other.m_keywords_by_name;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::copy_keywords
-//       Access: Public
-//  Description: Replaces this keyword list with those from the other
-//               list.
-////////////////////////////////////////////////////////////////////
-void DCKeywordList::
-copy_keywords(const DCKeywordList &other)
+// copy_keywords replaces this keyword list with those from the other list.
+void KeywordList::copy_keywords(const KeywordList &other)
 {
 	(*this) = other;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::add_keyword
-//       Access: Public
-//  Description: Adds the indicated keyword to the list.  Returns true
-//               if it is added, false if it was already there.
-////////////////////////////////////////////////////////////////////
-bool DCKeywordList::
-add_keyword(const DCKeyword *keyword)
+// add_keyword adds the indicated keyword to the list.
+//     Returns true if it is added, false if it was already there.
+bool KeywordList::add_keyword(const Keyword *keyword)
 {
-	bool inserted = _keywords_by_name.insert(KeywordsByName::value_type(keyword->get_name(),
-	                keyword)).second;
+	bool inserted = m_keywords_by_name.insert(
+		std::map<std::string, const Keyword*>::value_type(keyword->get_name(), keyword)).second;
 	if(inserted)
 	{
-		_keywords.push_back(keyword);
-		_flags |= keyword->get_historical_flag();
+		m_keywords.push_back(keyword);
+		m_flags |= keyword->get_historical_flag();
 	}
 
 	return inserted;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::clear_keywords
-//       Access: Public
-//  Description: Removes all keywords from the field.
-////////////////////////////////////////////////////////////////////
-void DCKeywordList::
-clear_keywords()
+// clear_keywords removes all keywords from the field.
+void KeywordList::clear_keywords()
 {
-	_keywords.clear();
-	_keywords_by_name.clear();
-	_flags = 0;
+	m_keywords.clear();
+	m_keywords_by_name.clear();
+	m_flags = 0;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::output_keywords
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-void DCKeywordList::
-output_keywords(ostream &out) const
+// output_keywords writes the keywords in the list to the output stream.
+void KeywordList::output_keywords(ostream &out) const
 {
-	Keywords::const_iterator ki;
-	for(ki = _keywords.begin(); ki != _keywords.end(); ++ki)
+	for(auto it = m_keywords.begin(); it != m_keywords.end(); ++it)
 	{
-		out << " " << (*ki)->get_name();
+		out << " " << (*it)->get_name();
 	}
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCKeywordList::generate_hash
-//       Access: Public
-//  Description: Accumulates the properties of these keywords into the
-//               hash.
-////////////////////////////////////////////////////////////////////
-void DCKeywordList::
-generate_hash(HashGenerator &hashgen) const
+// generate_hash accumulates the properties of these keywords into the hash.
+void KeywordList::generate_hash(HashGenerator &hashgen) const
 {
-	if(_flags != ~0)
+	if(m_flags != ~0)
 	{
 		// All of the flags are historical flags only, so add just the
 		// flags bitmask to keep the hash code the same as it has
 		// historically been.
-		hashgen.add_int(_flags);
+		hashgen.add_int(m_flags);
 
 	}
 	else
@@ -227,11 +140,13 @@ generate_hash(HashGenerator &hashgen) const
 		// There is at least one custom flag, so go ahead and make the
 		// hash code reflect it.
 
-		hashgen.add_int(_keywords_by_name.size());
-		KeywordsByName::const_iterator ni;
-		for(ni = _keywords_by_name.begin(); ni != _keywords_by_name.end(); ++ni)
+		hashgen.add_int(m_keywords_by_name.size());
+		for(auto it = m_keywords_by_name.begin(); it != m_keywords_by_name.end(); ++it)
 		{
-			(*ni).second->generate_hash(hashgen);
+			it->second->generate_hash(hashgen);
 		}
 	}
 }
+
+
+} // close namespace dclass
--- src/dclass/KeywordList.h
+++ src/dclass/KeywordList.h
@@ -1,64 +1,75 @@
-// Filename: dcKeywordList.h
-// Created by:  drose (25Jul05)
+// Filename: KeywordList.h
+// Created by: drose (25 Jul, 2005)
 //
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
 // Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
 // All use of this software is subject to the terms of the revised BSD
 // license.  You should have received a copy of this license along
 // with this source code in a file named "LICENSE."
 //
-////////////////////////////////////////////////////////////////////
 
-#ifndef DCKEYWORDLIST_H
-#define DCKEYWORDLIST_H
-
+#pragma once
 #include "dcbase.h"
+namespace dclass   // open namespace dclass
+{
 
-class DCKeyword;
+
+// Forward declaration
+class Keyword;
 class HashGenerator;
 
-////////////////////////////////////////////////////////////////////
-//       Class : DCKeywordList
-// Description : This is a list of keywords (see DCKeyword) that may
-//               be set on a particular field.
-////////////////////////////////////////////////////////////////////
-class EXPCL_DIRECT DCKeywordList
+// KeywordList this is a list of keywords (see Keyword) that may be set on a particular field.
+class EXPCL_DIRECT KeywordList
 {
 	public:
-		DCKeywordList();
-		DCKeywordList(const DCKeywordList &copy);
-		void operator = (const DCKeywordList &copy);
-		~DCKeywordList();
+		// empty list constructor
+		KeywordList();
+		// copy constructor
+		KeywordList(const KeywordList &copy);
+		// copy assignment operator
+		void operator = (const KeywordList &copy);
+		// destructor
+		~KeywordList();
 
 	PUBLISHED:
+		// has_keyword returns true if this list includes the indicated keyword, false otherwise.
 		bool has_keyword(const string &name) const;
-		bool has_keyword(const DCKeyword *keyword) const;
+		// has_keyword returns true if this list includes the indicated keyword, false otherwise.
+		bool has_keyword(const Keyword *keyword) const;
+		// get_num_keywords returns the number of keywords in the list.
 		int get_num_keywords() const;
-		const DCKeyword *get_keyword(int n) const;
-		const DCKeyword *get_keyword_by_name(const string &name) const;
+		// get_keyword returns the nth keyword in the list.
+		const Keyword *get_keyword(int n) const;
+		// get_keyword_by_name returns the requested keyword if it exists.
+		const Keyword *get_keyword_by_name(const string &name) const;
 
-		bool compare_keywords(const DCKeywordList &other) const;
+		// compare_keywords returns true if this list has the same keywords as the other list,
+		//     false if some keywords differ. Order is not considered important.
+		bool compare_keywords(const KeywordList &other) const;
 
 	public:
-		void copy_keywords(const DCKeywordList &other);
+		// copy_keywords replaces this keyword list with those from the other list.
+		void copy_keywords(const KeywordList &other);
 
-		bool add_keyword(const DCKeyword *keyword);
+		// add_keyword adds the indicated keyword to the list.
+		//     Returns true if it is added, false if it was already there.
+		bool add_keyword(const Keyword *keyword);
+
+		// clear_keywords removes all keywords from the field.
 		void clear_keywords();
 
+		// output_keywords writes the keywords in the list to the output stream.
 		void output_keywords(ostream &out) const;
+
+		// generate_hash accumulates the properties of these keywords into the hash.
 		void generate_hash(HashGenerator &hashgen) const;
 
 	private:
-		typedef pvector<const DCKeyword *> Keywords;
-		Keywords _keywords;
+		std::vector<const Keyword*> m_keywords; // the actual list of keywords
+		std::map<std::string, const Keyword*> m_keywords_by_name; // a map of name to keywords in list
 
-		typedef pmap<string, const DCKeyword *> KeywordsByName;
-		KeywordsByName _keywords_by_name;
-
-		int _flags;
+		int m_flags;
 };
 
-#endif
+
+} // close namespace dclass
