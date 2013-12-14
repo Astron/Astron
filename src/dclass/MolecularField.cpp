a0855e624cbc897d922ab5ba520298df33d0e38a
kestred <kestred@riotcave.com>
2013-12-14 09:59:20 -0700

    dclass: First iteration revision of MolecularField.

--- src/dclass/MolecularField.cpp
+++ src/dclass/MolecularField.cpp
@@ -1,250 +1,171 @@
-// Filename: dcMolecularField.cxx
-// Created by:  drose (05Oct00)
+// Filename: MolecularField.cpp
+// Created by: drose (05 Oct, 2000)
 //
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
 // Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
 // All use of this software is subject to the terms of the revised BSD
 // license.  You should have received a copy of this license along
 // with this source code in a file named "LICENSE."
 //
-////////////////////////////////////////////////////////////////////
 
-#include "dcMolecularField.h"
-#include "dcAtomicField.h"
-#include "hashGenerator.h"
-#include "dcindent.h"
-
-
-
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::Constructor
-//       Access: Public
-//  Description:
-////////////////////////////////////////////////////////////////////
-DCMolecularField::
-DCMolecularField(const string &name, DCClass *dclass) : DCField(name, dclass)
+#include "MolecularField.h"
+#include "AtomicField.h"
+#include "HashGenerator.h"
+#include "indent.h"
+namespace dclass   // open namespace dclass
 {
-	_got_keywords = false;
+
+
+// constructor
+MolecularField::MolecularField(const std::string &name, Class *dclass) : Field(name, dclass)
+{
+	m_got_keywords = false;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::as_molecular_field
-//       Access: Published, Virtual
-//  Description: Returns the same field pointer converted to a
-//               molecular field pointer, if this is in fact a
-//               molecular field; otherwise, returns NULL.
-////////////////////////////////////////////////////////////////////
-DCMolecularField *DCMolecularField::
-as_molecular_field()
+// as_molecular_field returns the same field pointer converted to a molecular field pointer,
+//     if this is in fact a molecular field; otherwise, returns NULL.
+MolecularField *MolecularField::as_molecular_field()
 {
 	return this;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::as_molecular_field
-//       Access: Published, Virtual
-//  Description: Returns the same field pointer converted to a
-//               molecular field pointer, if this is in fact a
-//               molecular field; otherwise, returns NULL.
-////////////////////////////////////////////////////////////////////
-const DCMolecularField *DCMolecularField::
-as_molecular_field() const
+// as_molecular_field returns the same field pointer converted to a molecular field pointer,
+//     if this is in fact a molecular field; otherwise, returns NULL.
+const MolecularField *MolecularField::as_molecular_field() const
 {
 	return this;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::get_num_atomics
-//       Access: Published
-//  Description: Returns the number of atomic fields that make up this
-//               molecular field.
-////////////////////////////////////////////////////////////////////
-int DCMolecularField::
-get_num_atomics() const
+// get_num_atomics returns the number of atomic fields that make up this molecular field.
+int MolecularField::get_num_atomics() const
 {
-	return _fields.size();
+	return m_fields.size();
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::get_atomic
-//       Access: Published
-//  Description: Returns the nth atomic field that makes up this
-//               molecular field.  This may or may not be a field of
-//               this particular class; it might be defined in a
-//               parent class.
-////////////////////////////////////////////////////////////////////
-DCAtomicField *DCMolecularField::
-get_atomic(int n) const
+// get_atomic returns the nth atomic field that makes up this molecular field.
+//     This may be an inherited field.
+AtomicField *MolecularField::get_atomic(int n) const
 {
-	nassertr(n >= 0 && n < (int)_fields.size(), NULL);
-	return _fields[n];
+	nassertr(n >= 0 && n < (int)m_fields.size(), NULL);
+	return m_fields[n];
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::add_atomic
-//       Access: Public
-//  Description: Adds the indicated atomic field to the end of the
-//               list of atomic fields that make up the molecular
-//               field.  This is normally called only during parsing
-//               of the dc file.  The atomic field should be fully
-//               defined by this point; you should not modify the
-//               atomic field (e.g. by adding more elements) after
-//               adding it to a molecular field.
-////////////////////////////////////////////////////////////////////
-void DCMolecularField::
-add_atomic(DCAtomicField *atomic)
+// add_atomic adds the indicated atomic field to the end of the list of atomic fields that
+//     make up the molecular field.  This is normally called only during parsing of the dc file.
+//     The atomic field should be fully defined by this point; you should not modify the
+//     atomic field (e.g. by adding more elements) after adding it to a molecular field.
+void MolecularField::add_atomic(AtomicField *atomic)
 {
 	if(!atomic->is_bogus_field())
 	{
-		if(!_got_keywords)
+		if(!m_got_keywords)
 		{
 			// The first non-bogus atomic field determines our keywords.
 			copy_keywords(*atomic);
-			_got_keywords = true;
+			m_got_keywords = true;
 		}
 	}
-	_fields.push_back(atomic);
+	m_fields.push_back(atomic);
 
 	int num_atomic_fields = atomic->get_num_nested_fields();
 	for(int i = 0; i < num_atomic_fields; i++)
 	{
-		_nested_fields.push_back(atomic->get_nested_field(i));
+		m_nested_fields.push_back(atomic->get_nested_field(i));
 	}
 
-	_num_nested_fields = _nested_fields.size();
+	m_num_nested_fields = m_nested_fields.size();
 
 	// See if we still have a fixed byte size.
-	if(_has_fixed_byte_size)
+	if(m_has_fixed_byte_size)
 	{
-		_has_fixed_byte_size = atomic->has_fixed_byte_size();
-		_fixed_byte_size += atomic->get_fixed_byte_size();
+		m_has_fixed_byte_size = atomic->has_fixed_byte_size();
+		m_fixed_byte_size += atomic->get_fixed_byte_size();
 	}
-	if(_has_fixed_structure)
+	if(m_has_fixed_structure)
 	{
-		_has_fixed_structure = atomic->has_fixed_structure();
+		m_has_fixed_structure = atomic->has_fixed_structure();
 	}
-	if(!_has_range_limits)
+	if(!m_has_range_limits)
 	{
-		_has_range_limits = atomic->has_range_limits();
+		m_has_range_limits = atomic->has_range_limits();
 	}
-	if(!_has_default_value)
+	if(!m_has_default_value)
 	{
-		_has_default_value = atomic->has_default_value();
+		m_has_default_value = atomic->has_default_value();
 	}
-	_default_value_stale = true;
+	m_default_value_stale = true;
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::output
-//       Access: Public, Virtual
-//  Description:
-////////////////////////////////////////////////////////////////////
-void DCMolecularField::
-output(ostream &out, bool brief) const
+// output writes a representation of the field to an output stream
+void MolecularField::output(std::ostream &out, bool brief) const
 {
 	out << _name;
 
-	if(!_fields.empty())
+	if(!m_fields.empty())
 	{
-		Fields::const_iterator fi = _fields.begin();
-		out << " : " << (*fi)->get_name();
-		++fi;
-		while(fi != _fields.end())
+		auto field_it = m_fields.begin();
+		out << " : " << (*field_it)->get_name();
+		++field_it;
+		while(field_it != m_fields.end())
 		{
-			out << ", " << (*fi)->get_name();
-			++fi;
+			out << ", " << (*field_it)->get_name();
+			++field_it;
 		}
 	}
 
 	out << ";";
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::write
-//       Access: Public, Virtual
-//  Description: Generates a parseable description of the object to
-//               the indicated output stream.
-////////////////////////////////////////////////////////////////////
-void DCMolecularField::
-write(ostream &out, bool brief, int indent_level) const
+// write writes a representation of the field to an output stream
+void MolecularField::write(std::ostream &out, bool brief, int indent_level) const
 {
 	indent(out, indent_level);
 	output(out, brief);
 	if(!brief)
 	{
-		out << "  // field " << _number;
+		out << "  // field " << m_number;
 	}
 	out << "\n";
 }
 
-
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::generate_hash
-//       Access: Public, Virtual
-//  Description: Accumulates the properties of this field into the
-//               hash.
-////////////////////////////////////////////////////////////////////
-void DCMolecularField::
-generate_hash(HashGenerator &hashgen) const
+// generate_hash accumulates the properties of this field into the hash.
+void MolecularField::generate_hash(HashGenerator &hashgen) const
 {
-	DCField::generate_hash(hashgen);
+	Field::generate_hash(hashgen);
 
-	hashgen.add_int(_fields.size());
-	Fields::const_iterator fi;
-	for(fi = _fields.begin(); fi != _fields.end(); ++fi)
+	hashgen.add_int(m_fields.size());
+	for(auto it = m_fields.begin(); it != m_fields.end(); ++it)
 	{
-		(*fi)->generate_hash(hashgen);
+		(*it)->generate_hash(hashgen);
 	}
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::get_nested_field
-//       Access: Public, Virtual
-//  Description: Returns the DCPackerInterface object that represents
-//               the nth nested field.  This may return NULL if there
-//               is no such field (but it shouldn't do this if n is in
-//               the range 0 <= n < get_num_nested_fields()).
-////////////////////////////////////////////////////////////////////
-DCPackerInterface *DCMolecularField::
-get_nested_field(int n) const
+// get_nested_field returns the PackerInterface object that represents the nth nested field.
+//     Will return null if the field is out of the range 0 <= n < get_num_nested_fields().
+PackerInterface *MolecularField::get_nested_field(int n) const
 {
-	nassertr(n >= 0 && n < (int)_nested_fields.size(), NULL);
-	return _nested_fields[n];
+	nassertr(n >= 0 && n < (int)m_nested_fields.size(), NULL);
+	return m_nested_fields[n];
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::do_check_match
-//       Access: Protected, Virtual
-//  Description: Returns true if the other interface is bitwise the
-//               same as this one--that is, a uint32 only matches a
-//               uint32, etc. Names of components, and range limits,
-//               are not compared.
-////////////////////////////////////////////////////////////////////
-bool DCMolecularField::
-do_check_match(const DCPackerInterface *other) const
+// do_check_match returns true if the other interface is bitwise the same as
+//     this one that is, a uint32 only matches a uint32, etc.
+//     Names of components, and range limits, are not compared.
+bool MolecularField::do_check_match(const PackerInterface *other) const
 {
 	return other->do_check_match_molecular_field(this);
 }
 
-////////////////////////////////////////////////////////////////////
-//     Function: DCMolecularField::do_check_match_molecular_field
-//       Access: Protected, Virtual
-//  Description: Returns true if this field matches the indicated
-//               molecular field, false otherwise.
-////////////////////////////////////////////////////////////////////
-bool DCMolecularField::
-do_check_match_molecular_field(const DCMolecularField *other) const
+// do_check_match_molecular_field returns true if this field matches the indicated molecular field.
+bool MolecularField::do_check_match_molecular_field(const MolecularField *other) const
 {
-	if(_nested_fields.size() != other->_nested_fields.size())
+	if(m_nested_fields.size() != other->m_nested_fields.size())
 	{
 		return false;
 	}
-	for(size_t i = 0; i < _nested_fields.size(); i++)
+	for(size_t i = 0; i < m_nested_fields.size(); i++)
 	{
-		if(!_nested_fields[i]->check_match(other->_nested_fields[i]))
+		if(!m_nested_fields[i]->check_match(other->_nested_fields[i]))
 		{
 			return false;
 		}
@@ -252,3 +173,6 @@ do_check_match_molecular_field(const DCMolecularField *other) const
 
 	return true;
 }
+
+
+} // close namespace dclass
--- src/dclass/MolecularField.h
+++ src/dclass/MolecularField.h
@@ -1,71 +1,78 @@
 // Filename: dcMolecularField.h
-// Created by:  drose (05Oct00)
+// Created by: drose (05 Oct, 2000)
 //
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
 // Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
 // All use of this software is subject to the terms of the revised BSD
 // license.  You should have received a copy of this license along
 // with this source code in a file named "LICENSE."
 //
-////////////////////////////////////////////////////////////////////
 
-#ifndef DCMOLECULARFIELD_H
-#define DCMOLECULARFIELD_H
-
+#pragma once
 #include "dcbase.h"
-#include "dcField.h"
+#include "Field.h"
+namespace dclass   // open namespace dclass
+{
 
-class DCAtomicField;
-class DCParameter;
 
-////////////////////////////////////////////////////////////////////
-//       Class : DCMolecularField
-// Description : A single molecular field of a Distributed Class, as
-//               read from a .dc file.  This represents a combination
-//               of two or more related atomic fields, that will often
-//               be treated as a unit.
-////////////////////////////////////////////////////////////////////
-class EXPCL_DIRECT DCMolecularField : public DCField
+// Forward declaration
+class AtomicField;
+class Parameter;
+
+// A MolecularField a single molecular field of a Distributed Class, as read from a .dc file.
+//     This represents a combination of two or more related atomic fields,
+//     that will often be treated as a unit.
+class EXPCL_DIRECT MolecularField : public Field
 {
 	public:
-		DCMolecularField(const string &name, DCClass *dclass);
+		MolecularField(const std::string &name, Class *dclass);
 
 	PUBLISHED:
-		virtual DCMolecularField *as_molecular_field();
-		virtual const DCMolecularField *as_molecular_field() const;
+		// as_molecular_field returns the same field pointer converted to a molecular field pointer,
+		//     if this is in fact a molecular field; otherwise, returns NULL.
+		virtual MolecularField *as_molecular_field();
+		virtual const MolecularField *as_molecular_field() const;
 
+		// get_num_atomics returns the number of atomic fields that make up this molecular field.
 		int get_num_atomics() const;
-		DCAtomicField *get_atomic(int n) const;
+		// get_atomic returns the nth atomic field that makes up this molecular field.
+		//     This may be an inherited field.
+		AtomicField *get_atomic(int n) const;
 
 	public:
-		void add_atomic(DCAtomicField *atomic);
 
+		// add_atomic adds the indicated atomic field to the end of the list of atomic
+		//     fields that make up the molecular field.
+		void add_atomic(AtomicField *atomic);
+
+		// output and write write a representation of the field to an output stream
 		virtual void output(ostream &out, bool brief) const;
 		virtual void write(ostream &out, bool brief, int indent_level) const;
+
+		// generate_hash accumulates the properties of this field into the hash.
 		virtual void generate_hash(HashGenerator &hashgen) const;
 
-		virtual DCPackerInterface *get_nested_field(int n) const;
+		// get_nested_field returns the PackerInterface object that represents the nth nested field.
+		//     Will return null if the field is out of the range 0 <= n < get_num_nested_fields().
+		virtual PackerInterface *get_nested_field(int n) const;
 
 	protected:
-		virtual bool do_check_match(const DCPackerInterface *other) const;
-		virtual bool do_check_match_molecular_field(const DCMolecularField *other) const;
+		// do_check_match returns true if the other interface is bitwise the same as
+		//     this one that is, a uint32 only matches a uint32, etc.
+		//     Names of components, and range limits, are not compared.
+		virtual bool do_check_match(const PackerInterface *other) const;
+		virtual bool do_check_match_molecular_field(const MolecularField *other) const;
 
 	private:
 		// These members define the primary interface to the molecular field
 		// definition as read from the file.
-		typedef pvector<DCAtomicField *> Fields;
-		Fields _fields;
-		bool _got_keywords;
+		std::vector<Field*> m_fields;
+		bool m_got_keywords;
 
-		DCParameter *get_next_pack_element();
+		Parameter* get_next_pack_element();
 
-		typedef pvector<DCPackerInterface *> NestedFields;
-		NestedFields _nested_fields;
+		std::vector<PackerInterface*> m_nested_fields;
 };
 
-#endif
 
-
+} // close namespace dclass
