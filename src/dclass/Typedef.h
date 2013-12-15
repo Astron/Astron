f2b65ab1eaee954d520ece7abcee1175704b764d
kestred <kestred@riotcave.com>
2013-12-14 21:30:53 -0700

    dclass: First iteration revision of Typedef.

--- src/dclass/Typedef.h
+++ src/dclass/Typedef.h
@@ -1,58 +1,53 @@
-// Filename: dcTypedef.h
-// Created by:  drose (17Jun04)
+// Filename: Typedef.h
+// Created by: drose (17 Jun, 2004)
 //
-////////////////////////////////////////////////////////////////////
-//
-// PANDA 3D SOFTWARE
 // Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
 // All use of this software is subject to the terms of the revised BSD
 // license.  You should have received a copy of this license along
 // with this source code in a file named "LICENSE."
 //
-////////////////////////////////////////////////////////////////////
 
-#ifndef DCTYPEDEF_H
-#define DCTYPEDEF_H
-
+#pragma once
 #include "dcbase.h"
-#include "dcDeclaration.h"
+#include "Declaration.h"
+namespace dclass   // open namespace dclass
+{
 
-class DCParameter;
 
-////////////////////////////////////////////////////////////////////
-//       Class : DCTypedef
-// Description : This represents a single typedef declaration in the
-//               dc file.  It assigns a particular type to a new name,
-//               just like a C typedef.
-////////////////////////////////////////////////////////////////////
-class EXPCL_DIRECT DCTypedef : public DCDeclaration
+// Forward declaration
+class Parameter;
+
+// A Typedef represents a single typedef declaration in the dc file.
+//     It assigns a particular type to a new name, ust like a C typedef.
+class EXPCL_DIRECT Typedef : public Declaration
 {
 	public:
-		DCTypedef(DCParameter *parameter, bool implicit = false);
-		DCTypedef(const string &name);
-		virtual ~DCTypedef();
+		Typedef(Parameter *parameter, bool implicit = false);
+		Typedef(const std::string &name);
+		virtual ~Typedef();
 
 	PUBLISHED:
 		int get_number() const;
-		const string &get_name() const;
-		string get_description() const;
+		const std::string &get_name() const;
+		std::string get_description() const;
 
 		bool is_bogus_typedef() const;
 		bool is_implicit_typedef() const;
 
 	public:
-		DCParameter *make_new_parameter() const;
+		Parameter *make_new_parameter() const;
 
 		void set_number(int number);
-		virtual void output(ostream &out, bool brief) const;
-		virtual void write(ostream &out, bool brief, int indent_level) const;
+		virtual void output(std::ostream &out, bool brief) const;
+		virtual void write(std::ostream &out, bool brief, int indent_level) const;
 
 	private:
-		DCParameter *_parameter;
-		bool _bogus_typedef;
-		bool _implicit_typedef;
-		int _number;
+		Parameter *_parameter;
+		bool m_bogus_typedef;
+		bool m_implicit_typedef;
+		int m_number;
 };
 
-#endif
+
+} // close namespace dclass
