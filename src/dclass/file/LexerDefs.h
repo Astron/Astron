56a3c7c5a6e5b890e0143db034e5c4c17e573508
kestred <kestred@riotcave.com>
2014-01-06 00:32:35 -0700

    dclass: Cleanup lexer.

--- src/dclass/file/LexerDefs.h
+++ src/dclass/file/LexerDefs.h
@@ -17,5 +17,11 @@ void init_value_lexer(std::istream &in, const std::string &filename);
 
 int run_lexer();
 
+int lexer_error_count();
+int lexer_warning_count();
+
+void lexer_error(const std::string &msg);
+void lexer_warning(const std::string &msg);
+
 // we always read files
 #define YY_NEVER_INTERACTIVE 1
