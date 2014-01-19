46635fa27dac40eca130c57c79dce6bd2dc57fa4
kestred <kestred@riotcave.com>
2014-01-18 17:28:51 -0700

    dclass: Fix parser_warning to call correct function.

--- src/dclass/file/write.h
+++ src/dclass/file/write.h
@@ -4,7 +4,6 @@
 namespace dclass   // open namespace dclass
 {
 
-
 // indent outputs the indicated number of spaces to the given output stream, returning the
 //     stream itself.  Useful for indenting a series of lines of text by a given amount.
 std::ostream& indent(std::ostream& out, unsigned int indent_level);
