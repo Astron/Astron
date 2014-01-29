838f40b9b3891dfbed02fe2c79a35a3f1ffa2e9f
kestred <kestred@riotcave.com>
2014-01-29 04:33:19 -0700

    Config: Implement validation for key-ed ConfigGroup lists (ie. "roles").

--- src/config/ConfigGroup.cpp
+++ src/config/ConfigGroup.cpp
@@ -28,12 +28,12 @@ bool ConfigGroup::validate(ConfigNode node)
 	{
 		if(m_name.length() > 0)
 		{
-			config_log.error() << "Category '" << m_path
+			config_log.error() << "Section '" << m_path
 			                    << "' has key/value config variables.\n";
 		}
 		else
 		{
-			config_log.error() << "Config categories must be at root of config file.\n";
+			config_log.error() << "Config sections must be at root of config file.\n";
 		}
 		return false;
 	}
@@ -59,12 +59,12 @@ bool ConfigGroup::validate(ConfigNode node)
 		{
 			if(m_name.length() > 0)
 			{
-				config_log.error() << "Category '" << m_path
+				config_log.error() << "Section '" << m_path
 				                    << "' has no attribute named '" << key << "'.\n";
 			}
 			else
 			{
-				config_log.error() << "Category '" << key << "' is not a valid config category.\n";
+				config_log.error() << "Section '" << key << "' is not a valid config category.\n";
 			}
 			ok = false;
 		}
@@ -84,7 +84,7 @@ bool ConfigList::validate(ConfigNode node)
 {
 	if(!node.IsSequence())
 	{
-		config_log.error() << "Category '" << m_path
+		config_log.error() << "Sec '" << m_path
 		                   << "' expects a list of values.\n";
 		return false;
 	}
@@ -114,7 +114,53 @@ KeyedConfigList::~KeyedConfigList()
 
 bool KeyedConfigList::validate(ConfigNode node)
 {
-	return true;
+	if(!node.IsSequence())
+	{
+		config_log.error() << "Section '" << m_path
+		                   << "' expects a list of values.\n";
+		return false;
+	}
+
+	int n = -1;
+	bool ok = true;
+	for(auto it = node.begin(); it != node.end(); ++it)
+	{
+		n += 1;
+
+		if(!it->IsMap())
+		{
+			config_log.error() << "The " << n << "th item of section '" << m_path
+			                   << "' does not have key/value config variables.\n";
+			ok = false;
+			continue;
+		}
+
+		ConfigNode element = *it;
+		if(!element[m_key])
+		{
+			config_log.error() << "The " << n << "th item of section '" << m_path
+			                   << "' did not specify the attribute '" << m_key << "'.\n";
+			ok = false;
+			continue;
+		}
+
+		string key = element[m_key].as<std::string>();
+		auto found_grp = m_children.find(key);
+		if(found_grp == m_children.end())
+		{
+			config_log.error() << "The value '" << key << "' is not valid for attribute '"
+			                   << m_key << "' of section '" << m_path << "'.\n";
+			ok = false;
+			continue;
+		}
+
+		if(!found_grp->second->validate(element))
+		{
+			ok = false;
+		}
+	}
+
+	return ok;
 }
 
 KeyedConfigGroup::KeyedConfigGroup(const string& key, KeyedConfigList& list) :
