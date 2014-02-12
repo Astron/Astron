aabf93b5f648d0b3435e08166e71a5bcecebe466
Alex Mault <alexskiing@mac.com>
2014-02-12 14:33:25 -0700

    Second Pass, Refactoring MDNetworkParticipant to use Datagram_ptr

--- src/messagedirector/MDNetworkParticipant.h
+++ src/messagedirector/MDNetworkParticipant.h
@@ -6,8 +6,8 @@ class MDNetworkParticipant : public MDParticipantInterface, public NetworkClient
 {
 	public:
 		MDNetworkParticipant(boost::asio::ip::tcp::socket *socket);
-		virtual void handle_datagram(Datagram &dg, DatagramIterator &dgi);
+		virtual void handle_datagram(Datagram_ptr &dg, DatagramIterator &dgi);
 	private:
-		virtual void receive_datagram(Datagram &dg);
+		virtual void receive_datagram(Datagram_ptr &dg);
 		virtual void receive_disconnect();
 };
