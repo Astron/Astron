cee981a04f44b5dbf3a970ce6e5840cfb397f50d
Sam Edwards <CFSworks@gmail.com>
2014-02-27 17:36:38 -0700

    Byteorder: Properly swap multibyte datatypes on big endian systems. 128-bit support coming later.

--- src/dclass/file/parser.h
+++ src/dclass/file/parser.h
@@ -1,19 +1,20 @@
-/* A Bison parser, made by GNU Bison 3.0.2.  */
+/* A Bison parser, made by GNU Bison 2.4.3.  */
 
-/* Bison interface for Yacc-like parsers in C
-
-   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
-
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
+   2009, 2010 Free Software Foundation, Inc.
+   
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,60 +27,55 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-
+   
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-#ifndef YY_YY_PARSER_H_INCLUDED
-# define YY_YY_PARSER_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 1
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
 
-/* Token type.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    UNSIGNED_INTEGER = 258,
-    REAL = 259,
-    STRING = 260,
-    HEX_STRING = 261,
-    IDENTIFIER = 262,
-    CHAR = 263,
-    START_DC_FILE = 264,
-    START_DC_VALUE = 265,
-    KW_DCLASS = 266,
-    KW_STRUCT = 267,
-    KW_FROM = 268,
-    KW_IMPORT = 269,
-    KW_TYPEDEF = 270,
-    KW_KEYWORD = 271,
-    KW_INT8 = 272,
-    KW_INT16 = 273,
-    KW_INT32 = 274,
-    KW_INT64 = 275,
-    KW_UINT8 = 276,
-    KW_UINT16 = 277,
-    KW_UINT32 = 278,
-    KW_UINT64 = 279,
-    KW_FLOAT32 = 280,
-    KW_FLOAT64 = 281,
-    KW_STRING = 282,
-    KW_BLOB = 283,
-    KW_CHAR = 284
-  };
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     UNSIGNED_INTEGER = 258,
+     REAL = 259,
+     STRING = 260,
+     HEX_STRING = 261,
+     IDENTIFIER = 262,
+     CHAR = 263,
+     START_DC_FILE = 264,
+     START_DC_VALUE = 265,
+     KW_DCLASS = 266,
+     KW_STRUCT = 267,
+     KW_FROM = 268,
+     KW_IMPORT = 269,
+     KW_TYPEDEF = 270,
+     KW_KEYWORD = 271,
+     KW_INT8 = 272,
+     KW_INT16 = 273,
+     KW_INT32 = 274,
+     KW_INT64 = 275,
+     KW_UINT8 = 276,
+     KW_UINT16 = 277,
+     KW_UINT32 = 278,
+     KW_UINT64 = 279,
+     KW_FLOAT32 = 280,
+     KW_FLOAT64 = 281,
+     KW_STRING = 282,
+     KW_BLOB = 283,
+     KW_CHAR = 284
+   };
 #endif
 
-/* Value type.  */
 
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
 
 extern YYSTYPE yylval;
 
-int yyparse (void);
 
-#endif /* !YY_YY_PARSER_H_INCLUDED  */
--- src/util/Datagram.h
+++ src/util/Datagram.h
@@ -7,6 +7,7 @@
 #include <stdexcept>
 #include <string.h> // memcpy
 #include "core/types.h"
+#include "dclass/util/byteorder.h"
 
 #ifdef ASTRON_32BIT_DATAGRAMS
 	typedef uint32_t dgsize_t;
@@ -161,7 +162,7 @@ class Datagram
 		void add_int16(const int16_t &v)
 		{
 			check_add_length(2);
-			*(int16_t *)(buf + buf_offset) = v;
+			*(int16_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 2;
 		}
 
@@ -169,7 +170,7 @@ class Datagram
 		void add_int32(const int32_t &v)
 		{
 			check_add_length(4);
-			*(int32_t *)(buf + buf_offset) = v;
+			*(int32_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 4;
 		}
 
@@ -177,7 +178,7 @@ class Datagram
 		void add_int64(const int64_t &v)
 		{
 			check_add_length(8);
-			*(int64_t *)(buf + buf_offset) = v;
+			*(int64_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 8;
 		}
 
@@ -193,7 +194,7 @@ class Datagram
 		void add_uint16(const uint16_t &v)
 		{
 			check_add_length(2);
-			*(uint16_t *)(buf + buf_offset) = v;
+			*(uint16_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 2;
 		}
 
@@ -201,7 +202,7 @@ class Datagram
 		void add_uint32(const uint32_t &v)
 		{
 			check_add_length(4);
-			*(uint32_t *)(buf + buf_offset) = v;
+			*(uint32_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 4;
 		}
 
@@ -209,7 +210,7 @@ class Datagram
 		void add_uint64(const uint64_t &v)
 		{
 			check_add_length(8);
-			*(uint64_t *)(buf + buf_offset) = v;
+			*(uint64_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 8;
 		}
 
@@ -217,7 +218,7 @@ class Datagram
 		void add_float32(const float &v)
 		{
 			check_add_length(4);
-			*(float *)(buf + buf_offset) = v;
+			*(float *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 4;
 		}
 
@@ -225,7 +226,7 @@ class Datagram
 		void add_float64(const double &v)
 		{
 			check_add_length(8);
-			*(double *)(buf + buf_offset) = v;
+			*(double *)(buf + buf_offset) = swap_le(v);
 			buf_offset += 8;
 		}
 
@@ -235,7 +236,7 @@ class Datagram
 		void add_size(const dgsize_t &v)
 		{
 			check_add_length(sizeof(dgsize_t));
-			*(dgsize_t *)(buf + buf_offset) = v;
+			*(dgsize_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += sizeof(dgsize_t);
 		}
 
@@ -245,7 +246,7 @@ class Datagram
 		void add_channel(const channel_t &v)
 		{
 			check_add_length(sizeof(channel_t));
-			*(channel_t *)(buf + buf_offset) = v;
+			*(channel_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += sizeof(channel_t);
 		}
 
@@ -255,7 +256,7 @@ class Datagram
 		void add_doid(const doid_t &v)
 		{
 			check_add_length(sizeof(doid_t));
-			*(doid_t *)(buf + buf_offset) = v;
+			*(doid_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += sizeof(doid_t);
 		}
 
@@ -265,7 +266,7 @@ class Datagram
 		void add_zone(const zone_t &v)
 		{
 			check_add_length(sizeof(zone_t));
-			*(zone_t *)(buf + buf_offset) = v;
+			*(zone_t *)(buf + buf_offset) = swap_le(v);
 			buf_offset += sizeof(zone_t);
 		}
 
@@ -275,9 +276,9 @@ class Datagram
 		void add_location(const doid_t &parent, const zone_t &zone)
 		{
 			check_add_length(sizeof(doid_t) + sizeof(zone_t));
-			*(doid_t *)(buf + buf_offset) = parent;
+			*(doid_t *)(buf + buf_offset) = swap_le(parent);
 			buf_offset += sizeof(doid_t);
-			*(zone_t *)(buf + buf_offset) = zone;
+			*(zone_t *)(buf + buf_offset) = swap_le(zone);
 			buf_offset += sizeof(zone_t);
 		}
 
