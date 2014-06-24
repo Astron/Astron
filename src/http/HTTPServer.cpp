9990332895b9202813346b63b184beec704cfb52
bobbybee <j104@rocketmail.com>
2014-06-24 14:22:31 -0400

    Request read

--- src/http/HTTPServer.cpp
+++ src/http/HTTPServer.cpp
@@ -1,11 +1,8 @@
 #include "HTTPServer.h"
 
-#include "core/global.h"
-#include <boost/bind.hpp>
-
 using boost::asio::ip::tcp;
 using namespace std;
-
+ 
 HTTPServer::HTTPServer(string str_ip, string str_port)
 {    
     tcp::resolver resolver(io_service);
@@ -23,15 +20,23 @@ HTTPServer::~HTTPServer() {
 
 void HTTPServer::start_accept()
 {
-    tcp::socket *socket = new tcp::socket(io_service);
-    tcp::endpoint peerEndpoint;
-	m_acceptor->async_accept(*socket, boost::bind(&HTTPServer::handle_accept,
-	                         this, socket, boost::asio::placeholders::error));
+    HTTPConnection::pointer new_connection = HTTPConnection::create(io_service);
+    
+   // tcp::socket *socket = new tcp::socket(io_service);
+    //tcp::endpoint peerEndpoint;
+	m_acceptor->async_accept(new_connection->socket(), 
+                            boost::bind(&HTTPServer::handle_accept,
+	                         this, new_connection, boost::asio::placeholders::error));
 }
 
-void HTTPServer::handle_accept(tcp::socket *socket, const boost::system::error_code& /*ec*/)
+void HTTPServer::handle_accept(HTTPConnection::pointer new_connection,           
+                            const boost::system::error_code& ec)
 {
-	boost::asio::ip::tcp::endpoint remote;
+    if(!ec) {
+        new_connection->start();
+    }
+    
+	/*boost::asio::ip::tcp::endpoint remote;
 	try
 	{
 		remote = socket->remote_endpoint();
@@ -43,15 +48,39 @@ void HTTPServer::handle_accept(tcp::socket *socket, const boost::system::error_c
 	}
     
     cout << "HTTP Server incoming connection: " << remote.address().to_string() << "\n";
-    
-    char* content = "<i>We're sorry, but Astron Web Administration is currently under construction. Nothing to see here!</i>";
-    
-    char data[2048];
-    sprintf(data, "HTTP/1.1 200 OK\nServer: Astron Web Administration\nContent-Type: text/html; charset=UTF-8;\nContent-Length: %d\nConnection: close\n\n%s\n",
-        strlen(content),
-        content); 
-    
-    socket->write_some(boost::asio::buffer(data, strlen(data)));
+        
+    socket->write_some(boost::asio::buffer(data, strlen(data)));*/
 
 	start_accept();
 }
+
+void HTTPConnection::start ()
+{
+    m_socket.async_read_some( boost::asio::buffer(m_request, MAX_HTTP_DATA_LENGTH), 
+                               boost::bind(&HTTPConnection::handle_read, shared_from_this(),
+                                            boost::asio::placeholders::error,
+                                            boost::asio::placeholders::bytes_transferred));
+}
+
+void HTTPConnection::handle_read(const boost::system::error_code& /* ec */,
+                    size_t /* bytes_transferred*/)
+{
+    
+    
+    stringstream content;
+    content << "<i>We're sorry, but Astron Web Administration is currently under construction. Nothing to see here!</i>"
+                            << m_request;
+    
+    stringstream s;
+    s << "HTTP/1.1 200 OK\nServer: Astron Web Administration\nContent-Type: text/html; charset=UTF-8;\nContent-Length: "
+         << content.str().length() << "\nConnection: close\n\n" 
+         << content.str() << "\n";
+    
+    boost::asio::async_write(m_socket, boost::asio::buffer(s.str()), 
+        boost::bind(&HTTPConnection::handle_write, shared_from_this(),
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+    
+}
+
+
--- src/http/HTTPServer.h
+++ src/http/HTTPServer.h
@@ -1,6 +1,50 @@
 #pragma once
 
+#include "core/global.h"
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
+
+#define MAX_HTTP_DATA_LENGTH 4096
+
+class HTTPConnection 
+    : public boost::enable_shared_from_this<HTTPConnection>
+{
+    public:
+        typedef boost::shared_ptr<HTTPConnection> pointer;
+        
+        static pointer create(boost::asio::io_service& io_service)
+        {
+          return pointer(new HTTPConnection(io_service));
+        }
+
+        boost::asio::ip::tcp::socket& socket()
+        {
+          return m_socket;
+        }
+        
+        void start ();
+        
+    private:
+        HTTPConnection(boost::asio::io_service& io_service)
+          : m_socket(io_service)
+        {
+            
+        }
+        
+        void handle_read(const boost::system::error_code& /* ec */,
+                            size_t /* bytes_transferred*/);
+        
+        void handle_write(const boost::system::error_code& /* ec */,
+                            size_t /* bytes_transferred*/)                
+        {
+            
+        }
+        
+        boost::asio::ip::tcp::socket m_socket;
+        char m_request[MAX_HTTP_DATA_LENGTH];
+};
 
 class HTTPServer
 {
@@ -12,9 +56,10 @@ class HTTPServer
         void start_accept();
         
         // client connected callback
-        void handle_accept(boost::asio::ip::tcp::socket *socket, 
+        void handle_accept(HTTPConnection::pointer new_connection, 
                             const boost::system::error_code &ec);
                             
     private:
         boost::asio::ip::tcp::acceptor *m_acceptor;
 };
+
