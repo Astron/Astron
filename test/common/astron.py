01ab0ca91ea16af0eaad0658f374b2a4cf7a9486
Psychotropos <truthbit2@gmail.com>
2018-04-14 18:46:52 +0100

    test: Wait until the process is actually dead before proceeding

--- test/common/astron.py
+++ test/common/astron.py
@@ -40,6 +40,7 @@ class Daemon(object):
         time.sleep(1.0) # Allow some time for daemon to finish up...
         if self.daemon is not None:
             self.daemon.kill()
+            self.daemon.wait()
         if self.config_file is not None:
             os.remove(self.config_file)
 
