4f584487aa26001cd0d0b39e9ad8afcce1ad4ff6
Altenius <jacobjm18@gmail.com>
2018-06-07 21:50:19 -0400

    stateserver: add operation counters
    completes state server item 1 in #387

--- src/stateserver/StateServer.h
+++ src/stateserver/StateServer.h
@@ -24,6 +24,14 @@ class StateServer : public Role
     std::unordered_map<uint16_t, prometheus::Gauge*> m_ss_objs_gauges;
     prometheus::Family<prometheus::Histogram>* m_ss_obj_size_builder = nullptr;
     std::unordered_map<uint16_t, prometheus::Histogram*> m_ss_obj_size_hist;
+    prometheus::Family<prometheus::Counter> *m_ss_obj_creation_builder = nullptr;
+    prometheus::Counter *m_ss_obj_creation_ctr = nullptr;
+    prometheus::Family<prometheus::Counter> *m_ss_obj_deletion_builder = nullptr;
+    prometheus::Counter *m_ss_obj_deletion_cnt = nullptr;
+    prometheus::Family<prometheus::Counter> *m_ss_obj_change_builder = nullptr;
+    prometheus::Counter *m_ss_obj_change_cnt = nullptr;
+    prometheus::Family<prometheus::Counter> *m_ss_obj_query_builder = nullptr;
+    prometheus::Counter *m_ss_obj_query_cnt = nullptr;
   private:
     void init_metrics();
     void handle_generate(DatagramIterator &dgi, bool has_other);
